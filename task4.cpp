bda65781869b4439b17d45008cd5b602fcdae52b
No4vick <MineBor1@yandex.ru>
2022-02-19 11:43:56 +0300

    Fixed task4

--- task4.cpp
+++ task4.cpp
@@ -21,18 +21,18 @@ void print32(void *p){
 
 void print64(void *p){
     
-    std::cout << "а) " << std::hex << *reinterpret_cast<unsigned long*>(p) << std::endl;
-    std::cout << "б) " << std::bitset<32>(*reinterpret_cast<unsigned long*>(p)) << std::endl;
-    std::cout << "в) " << std::dec << *reinterpret_cast<unsigned long*>(p) << std::endl;
-    std::cout << "г) " << std::hex << *reinterpret_cast<signed long*>(p) << std::endl;
-    std::cout << "д) " << std::bitset<32>(*reinterpret_cast<signed long*>(p)) << std::endl;
-    std::cout << "е) " << std::dec << *reinterpret_cast<signed long*>(p) << std::endl;
+    std::cout << "а) " << std::hex << *reinterpret_cast<unsigned long long*>(p) << std::endl;
+    std::cout << "б) " << std::bitset<64>(*reinterpret_cast<unsigned long long*>(p)) << std::endl;
+    std::cout << "в) " << std::dec << *reinterpret_cast<unsigned long long*>(p) << std::endl;
+    std::cout << "г) " << std::hex << *reinterpret_cast<signed long long*>(p) << std::endl;
+    std::cout << "д) " << std::bitset<64>(*reinterpret_cast<signed long long*>(p)) << std::endl;
+    std::cout << "е) " << std::dec << *reinterpret_cast<signed long long*>(p) << std::endl;
 
     std::cout.setf(std::ios::fixed, std::ios::floatfield);  // Устанавливает режим fixed notation (фиксированной нотации)
     std::cout.precision(2);  // Устанавливает точность после запятой
 
-    std::cout << "ж) " << std::fixed << *reinterpret_cast<float*>(p) << std::endl;
-    std::cout << "з) " << std::scientific << *reinterpret_cast<signed long*>(p) << std::endl; // scientific - научная нотация (с экспонентой)
+    std::cout << "ж) " << std::fixed << *reinterpret_cast<double*>(p) << std::endl;
+    std::cout << "з) " << std::scientific << *reinterpret_cast<signed long long*>(p) << std::endl; // scientific - научная нотация (с экспонентой)
     std::cout << std::endl;
 }
 
@@ -69,23 +69,23 @@ int main(){
 
     // print64
 
-    unsigned int val64;
+    unsigned long long val64;
 
     val64 = 0;
-    print64(reinterpret_cast<unsigned long *>(&val64));
+    print64(reinterpret_cast<unsigned long long *>(&val64));
 
-    val64 = ULONG_MAX;
-    print64(reinterpret_cast<unsigned long *>(&val64));
+    val64 = ULLONG_MAX;
+    print64(reinterpret_cast<unsigned long long *>(&val64));
 
-    val64 = LONG_MIN;
-    print64(reinterpret_cast<long *>(&val64));
+    val64 = LLONG_MIN;
+    print64(reinterpret_cast<long long *>(&val64));
 
-    val64 = LONG_MAX;
-    print64(reinterpret_cast<long *>(&val64));
+    val64 = LLONG_MAX;
+    print64(reinterpret_cast<long long *>(&val64));
 
-    unsigned long x_64 = 0x8A8B8C8D;
-    long y_64 = 6;
-    long z_64 = -3;
+    unsigned long long x_64 = 0x8A8B8C8D;
+    long long y_64 = 6;
+    long long z_64 = -3;
     double xf_64 = 0x8A8B8C8D;
     double yf_64 = 6;
     double zf_64 = -3;
@@ -98,5 +98,4 @@ int main(){
     print64(&xf_64);
     print64(&yf_64);
     print64(&zf_64);
-
 }
